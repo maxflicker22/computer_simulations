a448bd68046250587b1f253c8f910a9e3dfafb62
Max <markus.flicker@gmail.com>
2025-03-18 10:01:44 +0100

    1 fertig inklusive d

--- Exercise_1/exercise_1.py
+++ Exercise_1/exercise_1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
+import scipy.constants as const
 
 ### Exercise 1 - Density and velocity distribution of an ideal gas ###
 
@@ -14,7 +15,7 @@ V = 1
 n = 10
 x_min = 0
 x_max = 1
-bins = 10
+bins = 100
 
 
 def plot_histogram_with_errors(x_min, x_max, n, bins):
@@ -23,7 +24,7 @@ def plot_histogram_with_errors(x_min, x_max, n, bins):
 
     # Histogramm mit plt.hist()
     # PDF = integral über alle Werte muss eins sein, kleine bin width bedeutet hohe y werte
-    plt.figure(figsize=(8, 5))
+
     counts, bin_edges, _ = plt.hist(uniform_ran_numbers, bins=bins, density=True, alpha=0.7, color='b', edgecolor='k', label="Histogram (PDF)")
 
     # Bin-Mitte berechnen für die Fehlerbalken
@@ -65,14 +66,14 @@ plot_histogram_with_errors(x_min, x_max, 1000, bins)
 plot_histogram_with_errors(x_min, x_max, 100000, bins)
 
 
-# c)
+# c) & d)
 #Examine the fluctuations of the height of an individual bar
 
 
 
 def examine_fluctuations_of_heigt(N):
     M = 1000
-    bins = 10
+    bins = 500
     x_min = 0
     x_max = 1
 
@@ -94,6 +95,7 @@ def examine_fluctuations_of_heigt(N):
     # Convert lists to numpy arrays
     uni_ran_num_array = np.array(uni_ran_num_list)  # Shape: (M, N)
     counts_array = np.array(counts_list)  # Shape: (M, bins)
+    counts_not_normalized_array = counts_array * N * bin_width
 
 
     # Select bin index
@@ -105,7 +107,6 @@ def examine_fluctuations_of_heigt(N):
     std_bin_i = np.sqrt(variance_bin_i)  # Standard deviation
 
     # Generate histogram of bin heights
-    plt.figure(figsize=(8, 5))
     plt.hist(counts_array[:, i], bins=bins, density=True, alpha=0.7, color='g', edgecolor='k', label="Histogram (PDF)")
 
     # Generate Normal distribution (Gaussian curve) for overlay
@@ -126,7 +127,33 @@ def examine_fluctuations_of_heigt(N):
     plt.title(f"Histogram over M with N = {N}")
     plt.legend()
     plt.show()
-    plt.show()
+
+
+    # d)
+    # Calculate isothermal compressibility
+    V = 1
+    kB = const.k # Boltzmann k
+    T = 300 # Kelvin
+    Vi = V / bins
+    roh = N/V
+
+    # Caluclate Varianc and Exception Value of Ni
+    mean_Ni = np.mean(counts_not_normalized_array[:, i])
+    variance_Ni = np.var(counts_not_normalized_array[:, i])
+
+    # Isothermal compressibility approximation
+    kappa_T = (Vi / (kB * T)) * (variance_Ni / mean_Ni ** 2)
+
+    # Isothermal from ideal Gas
+    kappa_T_ideal = 1 / (roh * kB * T)
+
+    # Print results
+    print(f"Approximated isothermal compressibility: {kappa_T:.6e} (1/Pa)")
+    print(f"Ideal gas isothermal compressibility: {kappa_T_ideal:.6e} (1/Pa)")
+
+    # Compare results
+    difference = abs(kappa_T - kappa_T_ideal)
+    print(f"Difference: {difference:.6e} (1/Pa)")
 
 
 
