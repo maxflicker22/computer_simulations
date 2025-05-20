a4446080ba040ea8f93b26929c775860a2c75c5b
Max <markus.flicker@gmail.com>
2025-05-20 12:10:20 +0200

    two different temps

--- Exercisee_4/exercise_4_1.py
+++ Exercisee_4/exercise_4_1.py
@@ -5,7 +5,39 @@ import scipy.constants as const
 import time
 from numba import njit, prange
 
+#-----------Functions------------------
 
+def plot_initial_condition(particals_position, particals_velocity, packing_fraction, box_w, box_h):
+    print(f"Packing fraction: {packing_fraction:.2f}")
+    # OBERVATION: Packingfracion is when random between 0.5 and 0.6
+
+    # Plotting
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.set_xlim(0, box_w)
+    ax.set_ylim(0, box_h)
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
+    ax.set_title('Particles on Surface')
+    ax.set_aspect('equal', adjustable='box')
+    ax.grid(True)
+
+    # Plot velocities as arrows (quiver plot)
+    ax.quiver(
+        particals_position[:, 0],  # X positions
+        particals_position[:, 1],  # Y positions
+        particals_velocity[:, 0],  # vx components
+        particals_velocity[:, 1],  # vy components
+        angles='xy', scale_units='xy', scale=1, color='red', width=0.005
+    )
+
+    # Draw Particles and Circles
+    for pos in particals_position:
+        circle = plt.Circle(pos, radius, color='lightblue', fill=False, linestyle='--')
+        ax.add_patch(circle)
+        ax.plot(pos[0], pos[1], 'o', color='blue', markersize=5)
+    plt.tight_layout()
+    plt.savefig("initial_particals")
+    #plt.show()
 
 def is_overlapping(new_position, existing_positions, radius_of_particales, box_width, box_height):
     """
@@ -20,8 +52,7 @@ def is_overlapping(new_position, existing_positions, radius_of_particales, box_w
     distances = np.linalg.norm(raw_distances, axis=1) # distandces with periodic boundaries
     return np.any(distances < 2 * radius_of_particales)
 
-
-def initialize_particals_on_surface(number_of_particles, radius_of_particales, box_width, box_height, max_attempts=50000):
+def  initialize_particals_on_surface(number_of_particles, radius_of_particales, box_width, box_height, max_attempts=50000):
     """
     Initialize particles on the surface.
     """
@@ -58,7 +89,6 @@ def initialize_velocities_of_particals(number_of_particles, T = 1.0, kb = 1., m=
   
 
     return xy_velocity
-
 
 # Simplified Wang-Frenkel potential
 #@numba.njit
@@ -212,181 +242,129 @@ def calculate_distance_between_particals(positions, box_w, box_h):
     #print("Particals distances:", delta)
     return delta
 
-def evaluate_computational_cost():
-    N_values = np.arange(100, 3600, 500)  # Vary N from 100 to 1000
-    times = []
-    times_neighbours = []
 
-    
-
-    for N in N_values:
-        positions = np.random.uniform(0, box_h, [N, 2])
-
-
-        print(f"Evaluating for N = {N}")
-        start_time = time.perf_counter()
-        distances = calculate_distance_between_particals(positions, box_w, box_h)
-        end_time = time.perf_counter()
-        times.append(end_time - start_time)
-
-        print(f"Evaluating fork Neighbours N = {N}")
-        start_time = time.perf_counter()
-        cell_list, neighbor_cells = prepare_cell_lists(positions, box_w, box_h, rc)
-        partical_distances = compute_distances_with_cutoff_numba(positions, box_w, box_h, cell_list, neighbor_cells, rc)
-        end_time = time.perf_counter()
-        times_neighbours.append(end_time - start_time)
-
-    times = np.array(times) 
-    times_neighbours = np.array(times_neighbours) 
-    # Polynomial fit of degree 2 (quadratic)
-    coeffs = np.polyfit(N_values, times, 2)
-    fit_func = np.poly1d(coeffs)  # Create a callable polynomial function
-
-    # Plot results
-    plt.figure(figsize=(8, 5))
-    plt.plot(N_values, times, 'o-', label='Measured Time')
-    plt.plot(N_values, fit_func(N_values), 'r--', label=f'Quadratic Fit: {coeffs[0]:.2e}·N² + {coeffs[1]:.2e}·N + {coeffs[2]:.2e}')
-    plt.plot(N_values, times_neighbours, 'o-', label='Measured Time Neighbours')
-    plt.xlabel('Number of Particles (N)')
-    plt.ylabel('Time (s)')
-    plt.title('Computational Cost of Distance Calculation')
-    plt.grid(True)
-    plt.legend()
-    plt.savefig("Cost_Evaluaion")
-    #plt.show()
-
-def velocity_verlet(positions, velocities, forces, dt, box_size, cell_list, neighbour_map, m=1.0, kb = 1.0, rc=2.5):
+def velocity_verlet(positions, velocities, forces, dt, box_size, cell_list, neighbour_map, delta_thermostat, temperature, step, thermo_interval, m=1.0, kb = 1.0, rc=2.5):
     """One step of the velocity-Verlet algorithm."""
-
     # Half-step velocity update
     velocities_half = velocities.copy() +  0.5 * forces * dt / m
+
+    # Rescale Velocity - Thermostat
+    # Apply Heyes thermostat occasionally
+    if step % thermo_interval == 0:
+        kin_energy_half_step = 0.5 * m * np.sum(np.linalg.norm(velocities_half, axis=1)**2)
+        z = rescale_factor_heyes_thermostat(delta_thermostat)
+
+        if acceptance_prob_thermostat(kin_energy_half_step, z, temperature, kb, velocities.shape[1], velocities.shape[0]):
+            velocities_half *= z
 
     # Position update
     positions += velocities_half * dt
     positions %= box_size  # Periodic boundary conditions
 
-
+    # Calculate Force and Potential Energy
     forces_new, potential_energy = calculate_forces_and_potential_between_particals(positions, box_size[0], box_size[0], cell_list, neighbour_map, rc=rc)
-    #print("forces_new", forces_new)
+
     # Full-step velocity update
     velocities = velocities_half + 0.5 * forces_new * dt / m
-    #print("velocities", velocities)
+    
+    # Calculate Energy
     kinetic_energy = 0.5 * m * np.sum(np.linalg.norm(velocities, axis=1)**2)
-    
     total_energy = kinetic_energy + potential_energy
+    
+    # Calculate Temperature of the System
     N = positions.shape[0]
-    temperature = kinetic_energy / (N * kb)
+    d = velocities.shape[1]
+    Nf = d * N - d - 1
+    kinetic_temperature = (kinetic_energy) / (Nf * kb)
 
-    #print("kinetic_energy", kinetic_energy)
-    #print("total_energy", total_energy)
-    #print("temperature", temperature)
-    return positions, velocities, forces_new, potential_energy, kinetic_energy, total_energy, temperature
+    return positions, velocities, forces_new, potential_energy, kinetic_energy, total_energy, kinetic_temperature
 
     
+def rescale_factor_heyes_thermostat(delta):
+    """Apply Heyes rescaling to velocities."""
+    xi = np.random.uniform(-delta, delta) # -ln(delta)
+    z = np.exp(-xi)  
+    return z
+
+def acceptance_prob_thermostat(kin_energy, z, T, kB, d, N):
+    exponent = d * (N - 1)
+    weight = z**exponent * np.exp(-kin_energy * (z**2 - 1) / (kB * T))
+    return np.random.uniform() < min(1, weight)
+
+#----------------Programm--------------------------------------
 
 # Simulation Parameters
 num_particles = 5000
 radius = 1.
-# Simulation box dimensions
 box_w, box_h = 10, 10
-
+initial_T = 200.0
 m = 1.0
-T = 1.0  # Temperature in Kelvin
-kb = 1.  # Boltzmann constant
+kb = 1.0  # Boltzmann constant
 sigma = 1.0 # length scale
 epsilon = 1.0  # eneregy scale
 delta_t = 0.000001 * np.sqrt(m * sigma ** 2 / epsilon) # time step
 rc = 3. * sigma  # Cutoff radius for neighbor search
-# OBERVATION: Packingfracion is when random between 0.5 and 0.6
-
-particals_position, packing_fraction = initialize_particals_on_surface(num_particles, radius, box_w, box_h)
-particals_velocity = initialize_velocities_of_particals(len(particals_position))
-#print("particals_velocity", particals_velocity)
-print(f"Packing fraction: {packing_fraction:.2f}")
-#print(f" True numbre of particles: {len(particals_position)}")
-
-
-# Evaluate computational cost
-#evaluate_computational_cost()
-
-# Plotting
-fig, ax = plt.subplots(figsize=(6, 6))
-ax.set_xlim(0, box_w)
-ax.set_ylim(0, box_h)
-ax.set_xlabel('X-axis')
-ax.set_ylabel('Y-axis')
-ax.set_title('Particles on Surface')
-ax.set_aspect('equal', adjustable='box')
-ax.grid(True)
-
-# Plot velocities as arrows (quiver plot)
-ax.quiver(
-    particals_position[:, 0],  # X positions
-    particals_position[:, 1],  # Y positions
-    particals_velocity[:, 0],  # vx components
-    particals_velocity[:, 1],  # vy components
-    angles='xy', scale_units='xy', scale=1, color='red', width=0.005
-)
-
-# Draw Particles and Circles
-for pos in particals_position:
-    circle = plt.Circle(pos, radius, color='lightblue', fill=False, linestyle='--')
-    ax.add_patch(circle)
-    ax.plot(pos[0], pos[1], 'o', color='blue', markersize=5)
-plt.tight_layout()
-plt.savefig("initial_particals")
-#plt.show()
-
-
-
-num_steps = 100000
-
-
-time_total = np.arange(num_steps) * delta_t
-
-kin_energies = np.zeros(num_steps)
-pot_energies = np.zeros(num_steps)
-tot_energies = np.zeros(num_steps)
-tempeeratures = np.zeros(num_steps)
-
-positions = particals_position.copy()
-velocities = particals_velocity.copy()
-
 rc_skin = 1. * rc
 rc_de = rc + rc_skin
+delta_thermostat = 0.015
+num_steps = 10000
+time_total = np.arange(num_steps) * delta_t
+thermo_interval = 2
 
-last_positions = positions.copy()
+kin_energies = {}
+pot_energies = {}
+tot_energies = {}
+kinetic_temperature = {}
+given_temperatures = [100.0, 350.0]
 
-cell_list, neighbour_map = prepare_cell_lists(positions, box_w, box_h, rc_de)
 
-forces, potential = calculate_forces_and_potential_between_particals(positions, box_w, box_h, cell_list, neighbour_map, rc)
+# Calculate initial particals position, velocity etc.
+particals_position, packing_fraction = initialize_particals_on_surface(num_particles, radius, box_w, box_h)
+particals_velocity = initialize_velocities_of_particals(len(particals_position), initial_T, kb, m)
 
+# Plot initial conditions
+plot_initial_condition(particals_position, particals_velocity, packing_fraction, box_w, box_h)
 
-for i in range(num_steps):
+# Sart Simulation
+for temp in given_temperatures:
+    # Set initial positions and velocities
+    positions = particals_position.copy()
+    velocities = particals_velocity.copy()  
+    last_positions = positions.copy()
 
-    print("current step:", i)
+    # Initialize dictionarys
+    kin_energies[temp] = np.zeros(num_steps)
+    pot_energies[temp] = np.zeros(num_steps)
+    tot_energies[temp] = np.zeros(num_steps)
+    kinetic_temperature[temp] = np.zeros(num_steps)
 
-    
-    #print("forces", forces)
-    #print("positions", positions)
-    #print("velocities", velocities)
-    max_displacement = np.max(np.linalg.norm(positions - last_positions, axis=1))
-
-    if max_displacement > rc_skin / 4:
-        cell_list, neighbour_map = prepare_cell_lists(positions, box_w, box_h, rc_de)
-        last_positions = positions.copy()  # Reset displacement tracking
-
+    # Calculate cell_list and neighbour list for the first time
     cell_list, neighbour_map = prepare_cell_lists(positions, box_w, box_h, rc_de)
-    positions, velocities, forces, pot_energies[i], kin_energies[i], tot_energies[i], tempeeratures[i] = velocity_verlet(positions, velocities, forces, delta_t, [box_w, box_h], cell_list, neighbour_map, rc=rc)
-    #print("pot_energies", pot_energies.shape)
+    
+    # initial forces and potential
+    forces, potential = calculate_forces_and_potential_between_particals(positions, box_w, box_h, cell_list, neighbour_map, rc)
+
+    for i in range(num_steps):
+        print("current step:", i)
+        # max displacement for adjusting neighbourts_list (Optional)
+        max_displacement = np.max(np.linalg.norm(positions - last_positions, axis=1))
+        if max_displacement > rc_skin / 8:
+            cell_list, neighbour_map = prepare_cell_lists(positions, box_w, box_h, rc_de)
+            last_positions = positions.copy()  # Reset displacement tracking
+
+        #cell_list, neighbour_map = prepare_cell_lists(positions, box_w, box_h, rc_de)
+        # make Timestep and return necessary Variables
+        positions, velocities, forces, pot_energies[temp][i], kin_energies[temp][i], tot_energies[temp][i], kinetic_temperature[temp][i] = velocity_verlet(positions, velocities, forces, delta_t, [box_w, box_h], cell_list, neighbour_map, delta_thermostat, temp, i, thermo_interval, rc=rc)
 
 
+#-----------------Plot Results---------------
 
-
+# Plot Energies
 plt.figure(figsize=(10, 6))
-plt.plot(time_total, kin_energies, label="Kinetic Energy", linestyle='-', marker='o', markersize=3)
-plt.plot(time_total, pot_energies, label="Potential Energy", linestyle='-', marker='x', markersize=3)
-plt.plot(time_total, tot_energies, label="Total Energy", linestyle='-', marker='s', markersize=3)
+for temp in given_temperatures:
+    plt.plot(time_total, kin_energies[temp], label=f"Kinetic Energy T={temp}")
+    plt.plot(time_total, pot_energies[temp], label=f"Potential Energy T={temp}")
+    plt.plot(time_total, tot_energies[temp], label=f"Total Energy T={temp}")
 plt.xlabel("Time [s]")
 plt.ylabel("Energy [J]")
 plt.title("Energy vs Time")
@@ -396,9 +374,11 @@ plt.tight_layout()
 plt.savefig("Energy Balance")
 #plt.show()
 
-
+# Plot Temperature
 plt.figure(figsize=(8, 5))
-plt.plot(time_total, tempeeratures, color='orange', linestyle='-', marker='d', markersize=3)
+for temp in given_temperatures:
+    plt.plot(time_total, kinetic_temperature[temp], linestyle='-', marker='d', markersize=3, label=f"T={temp}")
+plt.legend()
 plt.xlabel("Time [s]")
 plt.ylabel("Temperature [K or Reduced Units]")
 plt.title("Temperature vs Time")
@@ -407,5 +387,9 @@ plt.tight_layout()
 plt.savefig("Temperature")
 #plt.show()
 
-
 # %%
+
+
+#----------------------End----------------------
+
+
