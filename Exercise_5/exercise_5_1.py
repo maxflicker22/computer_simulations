9f6416bff99d0ae0bd0d0cdb4ab3ae1989238ec7
Max <markus.flicker@gmail.com>
2025-06-11 19:10:32 +0200

    asdf

--- Exercise_5/exercise_5_1.py
+++ Exercise_5/exercise_5_1.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#--------------------------------- Plot Functions ---------------------------------
 
-def plot_acceptance_rate(ks, acceptance_rate):
+def plot_acceptance_rate(ks, acceptance_rate, alternative_pacc=False):
     """
     Plot the acceptance rate against the k values.
     """
@@ -12,9 +13,17 @@ def plot_acceptance_rate(ks, acceptance_rate):
     plt.ylabel('Acceptance Rate')
     plt.title('Acceptance Rate vs. k values')
     plt.grid(True)
+    plt.tight_layout()
+    filename = 'acceptance_rate_vs_k.png'
+    if alternative_pacc:
+        pacc = "alternative"
+    else:
+        pacc = "standard"
+    filename = f"q:{q}_L:{steps_per_temperature}_pacc:{pacc}" + filename
+    plt.savefig(filename)  # Save the figure as an image file
     plt.show()
 
-def plot_energie_datas(beta_k, min_energies, average_energies, scaled_variance_energies, Energies_list):
+def plot_energie_datas(beta_k, min_energies, average_energies, scaled_variance_energies, Energies_list, alternative_pacc=False):
     # Convert beta_k and energy lists to numpy arrays for consistent plotting
     betas = np.array(beta_k)  # Skip the first beta (beta_start)
     min_energies = np.array(min_energies)
@@ -74,8 +83,18 @@ def plot_energie_datas(beta_k, min_energies, average_energies, scaled_variance_e
 
     plt.suptitle('Energy Metrics vs. Inverse Temperature (Simulated Annealing)', fontsize=16)
     plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+    filename = 'energy_metrics_vs_inverse_temperature.png'
+
+    if alternative_pacc:
+        pacc = "alternative"
+    else:
+        pacc = "standard"
+    filename = f"q:{q}_L:{steps_per_temperature}_pacc:{pacc}" + filename
+
+    plt.savefig(filename)  # Save the figure as an image file
     plt.show()
 
+#--------------------------------- Programm Functions ---------------------------------
 
 def create_initial_city_positions(num_cities, box_size, seed):
     """
@@ -86,22 +105,36 @@ def create_initial_city_positions(num_cities, box_size, seed):
     return cities , np.arange(num_cities)
 
 
-def plot_cities(cities, box_size, radius_of_city, best_path_flag=False):
+def plot_cities(cities, box_size, radius_of_city, best_path_flag=False, alternative_pacc=False):
     """
     Plot the cities on a 2D plane.
     """
+    path_distance = calculate_distance_of_Salesman(cities)
     plt.figure(figsize=(8, 8))
     plt.xlim(0, box_size)
     plt.ylim(0, box_size)
+    filename = 'city_positions.png'
     if best_path_flag:
-        plt.plot(cities[:, 0], cities[:, 1], s=radius_of_city*100, c='blue', alpha=0.5, label='Best Path')
+        plt.plot(cities[:, 0], cities[:, 1], c='blue', alpha=0.5, label='Path')
+        plt.scatter(cities[:, 0], cities[:, 1], s=radius_of_city*100, c='blue', alpha=0.5, label='Cities')
+        plt.title('City Positions with Path\nTotal Distance: {:.2f}'.format(path_distance))
+        filename = 'city_positions_best_path.png'
     else:
         plt.scatter(cities[:, 0], cities[:, 1], s=radius_of_city*100, c='blue', alpha=0.5, label='Cities')
-    plt.title('City Positions')
+        plt.title('City Positions')
+        filename = 'city_positions_initial_path.png'
+
+    if alternative_pacc:
+        pacc = "alternative"
+    else:
+        pacc = "standard"
+    filename = f"q:{q}_L:{steps_per_temperature}_pacc:{pacc}" + filename
     plt.xlabel('X Position')
     plt.ylabel('Y Position')
     plt.grid()
     plt.legend()
+    plt.tight_layout()
+    plt.savefig(filename)  # Save the figure as an image file^
     plt.show()
     #print("Initial city positions created successfully.")
     #print("Number of cities created:", len(cities))
@@ -135,14 +168,21 @@ def propose_new_city_configuration(order):
     #print(f"Proposed new order: {new_order}")
     return new_order
 
-def acceptance_probability(old_distance, new_distance, beta):
+def acceptance_probability(old_distance, new_distance, beta, alternative_pacc=False):
     """
     Calculate the acceptance probability for the new configuration.
     """
     if new_distance < old_distance:
         return 1.0
     else:
-        return np.exp((old_distance - new_distance) * beta)
+        if alternative_pacc:
+            # Alternative acceptance probability calculation
+            return 1 / (1 + np.exp(beta * (new_distance - old_distance)))
+        else:
+            # Standard acceptance probability calculation
+            #print(f"Old distance: {old_distance}, New distance: {new_distance}, Beta: {beta}")
+            #print(f"Acceptance probability: {np.exp((old_distance - new_distance) * beta)}")
+            return np.exp((old_distance - new_distance) * beta)
 
 def try_update_order(cities, old_distance, order, beta):
     """
@@ -152,7 +192,7 @@ def try_update_order(cities, old_distance, order, beta):
     new_order = propose_new_city_configuration(order)
     new_distance = calculate_distance_of_Salesman(cities[new_order])
     
-    prob = acceptance_probability(old_distance, new_distance, beta)
+    prob = acceptance_probability(old_distance, new_distance, beta, alternative_pacc=alternative_pacc)
     
     if np.random.rand() < prob:
         #print("Accepted new configuration.")
@@ -184,29 +224,77 @@ def discuss_energies(energies_at_specific_beta, beta):
     scaled_variance_energy = variance_energy * beta ** 2
     return  min_energy, average_energy, scaled_variance_energy
 
-def convegence_check(energies, threshold=1e-10):
+def convergence_check(energies, threshold, step, threshold_acceptance_rate):
     """
     Check if the energies have converged.
+    Criteria:
+    At least 10 energy differences exist.
+    Each difference is below the threshold.
+    Each previous difference is >= the following one (monotonically decreasing).
+    Acceptance rate is below the threshold_acceptance_rate.
     """
-    if len(energies) < 2:
+    windowsize = 2  # Number of differences to check
+    if len(energies) < windowsize:  # Need at least 11 energies to get 10 differences
         return False
-    return np.abs(energies[-1] - energies[-2]) < threshold
+    if step < windowsize:
+        return False
+
+    # Calculate the last 10 differences
+    diffs = np.abs(np.diff(energies[-windowsize:]))  # this gives 10 differences
+
+    # Check if all differences are below threshold
+    if not np.all(diffs < threshold):
+        return False
+
+    # Check if differences are monotonically decreasing
+    if not np.all(diffs[:-1] >= diffs[1:]):
+        return False
+
+    # Calculate acceptance rate
+    rate = len(energies) / step
+    if rate >= threshold_acceptance_rate:
+        return False
+
+    print("Convergence check: Energies have converged.")
+    print("Last differences:", diffs)
+    print("Acceptance rate:", rate)
+
+    return True
+
+
     
 
+def acceptance_rate_check(accepted_moves, total_moves, threshold):
+    """
+    Check if the acceptance rate is below a threshold.
+    """
+    rate = accepted_moves / total_moves
+    return rate < threshold
+    
 
+#--------------------------------- Programm---------------------------------
 
 
 # Define parameters 
+
+#### Fixed Parameters von hier
 box_size = 1.0  # Size of the box
 num_cities = 50  # Number of cities
 radius_of_city = 1.0  # Radius of each city
 seed = 42  # Random seed for reproducibility
 beta_start = 1 # Inverse temperature parameter
+#### bis hier
+
+# Initialize variables
 beta_k = []
-ks = np.arange(1, 20)  # Range of k values for beta update
-#print("ks:", ks)
-q = 1
+ks = np.arange(1, 100)  # Range of k values for beta update
+q = 1.0 # Cooling rate
 steps_per_temperature = 1 * (num_cities ** 2)
+threshold = 1e-4  # Convergence threshold
+threshold_acceptanc_rate = 0.1#
+
+alternative_pacc = False  # Use alternative acceptance probability calculation
+
 # Initialize the list to store energies for each beta value
 Energies_list = {}
 
@@ -216,8 +304,6 @@ average_energies = []
 scaled_variance_energies = []
 acceptance_rate = []
 
-
-
 # Create initial city positions and order
 cities, order = create_initial_city_positions(num_cities, box_size, seed=seed)
 # Calculate the initial distance
@@ -226,9 +312,8 @@ old_disance = calculate_distance_of_Salesman(cities[order])
 # Delete random seed to avoid confusion
 np.random.seed(1234)
 
+# Conveergence flag
 convergence_reached = False
-
-print("ks", ks)
 
 # Start loop over cooling steps
 for k in ks:
@@ -260,7 +345,8 @@ for k in ks:
         #print("Current Step:", step + 1, "/", steps_per_temperature)
 
         # Check for convergence
-        if convegence_check(Energies_list[beta_k[-1]]):
+        if convergence_check(Energies_list[beta_k[-1]], threshold, step + 1, threshold_acceptanc_rate):
+        #if acceptance_rate_check(len(Energies_list[beta_k[-1]]), step + 1, threshold_acceptanc_rate):   
             print("Convergence reached at step", step + 1)
             print("Final order of cities:", order)
             print("Final distance:", Energies_list[beta_k[-1]][-1])
@@ -293,20 +379,19 @@ for k in ks:
         print("Nothing converged anymore, stopping the simulation.")
 
 
-
-print("min energies", min_energies)
+#--------------------------------- Plot Execution---------------------------------
 
 # Plotting the cities
-plot_cities(cities, box_size, radius_of_city)
+plot_cities(cities, box_size, radius_of_city, best_path_flag=True, alternative_pacc=alternative_pacc)
 
 # Plot the energy data
-plot_energie_datas(beta_k, min_energies, average_energies, scaled_variance_energies, Energies_list)
+plot_energie_datas(beta_k, min_energies, average_energies, scaled_variance_energies, Energies_list, alternative_pacc=alternative_pacc)
 
 # Plot the acceptance rate
-plot_acceptance_rate(ks[:len(acceptance_rate)], acceptance_rate)
+plot_acceptance_rate(ks[:len(acceptance_rate)], acceptance_rate, alternative_pacc=alternative_pacc)
 
 # Plotting the cities
-plot_cities(cities[order], box_size, radius_of_city, best_path_flag=True)
+plot_cities(cities[order], box_size, radius_of_city, best_path_flag=True, alternative_pacc=alternative_pacc)
 
 
 
